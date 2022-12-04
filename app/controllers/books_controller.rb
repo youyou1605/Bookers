99e6a4530fcd4b5bc6a9ea6df5afab1183d9c7b6
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-04 16:36:38 +0000

    [Made]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
-  def top
+  def new
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
+ 
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def book_params
+   params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'homes/top'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
