268d4e345a4c425c7647e71228ea4dc2a44b16e0
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-05 12:55:32 +0000

    [Made]show,edit,update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,23 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
- 
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+    def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    end
 
   private
 
