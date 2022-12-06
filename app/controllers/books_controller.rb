a02ad8b7a436cdff35728e132659b03f8a92ab31
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-06 14:33:40 +0000

    [Made]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :index
+  end
   end
 
   def show
@@ -19,11 +24,16 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :edit
   end
-    
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
